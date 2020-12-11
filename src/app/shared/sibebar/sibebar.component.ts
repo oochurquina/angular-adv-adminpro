5d61c6696313936b9c3b1aa3e9a5e8748abda7b5
OMAR <oochurquina@hotmail.com>
2020-12-11 19:18:04 -0300

    Fin seccion 3 - diseño

---
+++ src/app/shared/sibebar/sibebar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sibebar',
+  templateUrl: './sibebar.component.html',
+  styles: [
+  ]
+})
+export class SibebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
